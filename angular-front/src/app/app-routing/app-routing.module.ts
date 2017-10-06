d329e612c7460ee95b545d7ff74cfaed32be4128
Alain Daussin <a.daussin@afelio.be>
2017-10-06 19:16:45 +0200

    Add route to menu-page

--- angular-front/src/app/app-routing/app-routing.module.ts
+++ angular-front/src/app/app-routing/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "../home/home.component";
+import { MenuPageComponent } from '../menu-page/menu-page.component';
 
 const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'menu', component: MenuPageComponent}
 ];
 
 @NgModule({
