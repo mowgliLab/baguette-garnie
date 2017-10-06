008b1aeb338534a473a869f0a4ffa1a82a0d6948
Alain Daussin <a.daussin@afelio.be>
2017-10-06 19:16:14 +0200

    add MenuPageComponent

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MenuPageComponent } from './menu-page/menu-page.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        NavbarComponent
+        NavbarComponent,
+        MenuPageComponent
     ],
     imports: [
         BrowserModule,
