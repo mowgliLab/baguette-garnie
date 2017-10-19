fc9b1afcf9abb46a0091d3dc3435262fba1719a0
Alain Daussin <a.daussin@afelio.be>
2017-10-19 20:51:20 +0200

    Business logic for saving custom sandwich

--- bg-back-end/src/business-logic/sandwich.bl.ts
+++ bg-back-end/src/business-logic/sandwich.bl.ts
@@ -1,6 +1,7 @@
 import { SandwichModel } from '../models/sandwich.model';
 import { SandwichEntity } from '../entyties/sandwich.entity';
 import { getRepository } from 'typeorm';
+import { UserModel } from '../models/user.model';
 
 export class SandwichBl {
 
@@ -17,6 +18,17 @@ export class SandwichBl {
 
     public getSandwiches(): SandwichModel {
         return new SandwichModel();
+    }
+
+    public saveCustomSandwich(sandwich: SandwichModel, user: UserModel): Promise<any> {
+        const sandwichRepository = getRepository(SandwichEntity);
+        const sandwichEntity = SandwichModel.toEntity(sandwich, true);
+        sandwichEntity.user = UserModel.toEntity(user);
+        return sandwichRepository
+            .save(sandwichEntity)
+            .then(res => res)
+            .catch(err => false);
+
     }
 
 }
