8e909ad29448b2106c1123c2a673ccd58b17ceef
Alain Daussin <a.daussin@afelio.be>
2017-10-21 10:37:05 +0200

    Fix size values

--- angular-front/src/app/models/sandwich.model.ts
+++ angular-front/src/app/models/sandwich.model.ts
@@ -2,12 +2,12 @@ import { BreadModel } from './bread.model';
 import { ToppingModel } from './topping.model';
 
 export class SandwichModel {
-    public static readonly sizeValues = {
-        1: {name: '1/4', value: 0.25},
-        2: {name: '1/2', value: 0.5},
-        3: {name: '3/4', value: 0.75},
-        4: {name: 'baguette entière', value: 1}
-    };
+    public static readonly sizeValues = [
+        {name: '1/4', value: 0},
+        {name: '1/2', value: 1},
+        {name: '3/4', value: 2},
+        {name: 'baguette entière', value: 3}
+    ];
 
     public id: number;
     public name: string;
