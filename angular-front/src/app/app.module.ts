9cfb7da932579cc96cfd7e1e165e7244adbd3266
Alain Daussin <a.daussin@afelio.be>
2017-10-21 10:38:01 +0200

    Work on nav bar to display different status if user is logged or not and display orders status.

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -12,12 +12,14 @@ import { HttpModule } from '@angular/http';
 import { SandwichElementComponent } from './components/sandwich-element/sandwich-element.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
 import { TeamPageComponent } from './components/team-page/team-page.component';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
 import { SandwichService } from './services/sandwich.service';
 import { CustomSandwichComponent } from './components/custom-sandwich/custom-sandwich.component';
 import { ToppingService } from './services/topping.service';
 import { BreadService } from './services/bread.service';
+import { MemoryService } from './services/memory.service';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 @NgModule({
     declarations: [
@@ -35,14 +37,18 @@ import { BreadService } from './services/bread.service';
         AppRoutingModule,
         HttpModule,
         FormsModule,
-        ModalModule.forRoot()
+        ReactiveFormsModule,
+        ModalModule.forRoot(),
+        BsDropdownModule.forRoot()
     ],
     providers: [
         MenuService,
         SandwichService,
         ToppingService,
         BreadService,
-        BsModalService
+        BsModalService,
+        MemoryService,
+        FormBuilder
     ],
     bootstrap: [AppComponent]
 })
--- angular-front/src/app/components/navbar/navbar.component.ts
+++ angular-front/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { MemoryService } from '../../services/memory.service';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    orderSandwichCount: number;
+    isLoggedIn: boolean;
 
-  ngOnInit() {
-  }
+    constructor(private memoryService: MemoryService) {
+    }
+
+    ngOnInit() {
+        this.memoryService.orderSandwichesCount.subscribe(count => this.orderSandwichCount = count);
+        this.memoryService.isLoggedIn.subscribe(res => this.isLoggedIn = res);
+    }
+
+    loggin(newValue: boolean) {
+        this.memoryService.setIsLoggedIn(newValue);
+    }
 
 }
